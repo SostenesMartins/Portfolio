445fbfac3879d928399695d3c29902017b30d08f
SostenesMartins <sostenesmartins1996@gmail.com>
2023-05-18 00:52:50 -0300

    update header with css

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const burger = document.querySelector('.menu__icon');
+const menu = document.querySelector('.menu');
+
+if (burger && menu) {
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('_active');
+        menu.classList.toggle('_active');
+        body.classList.toggle('_lock');
+    })
+}
